97eada69406212a07777f7e89c35d44091846bfe
almaduri <ali.mdrw@gmail.com>
2022-02-23 09:47:37 +0800

    add The World's Finest Assassin Gets Reincarnated in Another World as an Aristocrat

--- watched_anime.sql
+++ watched_anime.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2022 at 02:15 AM
+-- Generation Time: Feb 23, 2022 at 02:47 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.5
 
@@ -184,7 +184,8 @@ INSERT INTO `watched_anime` (`id`, `title`) VALUES
 (145, 'Boku no Hero Academia the Movie 3: World Heroes\' Mission'),
 (146, 'Irina: The Vampire Cosmonaut'),
 (147, 'The Fruit of Evolution: Before I Knew It, My Life Had It Made'),
-(148, 'Takt Opt. Destiny');
+(148, 'Takt Opt. Destiny'),
+(149, 'The World\'s Finest Assassin Gets Reincarnated in Another World as an Aristocrat');
 
 --
 -- Indexes for dumped tables
@@ -204,7 +205,7 @@ ALTER TABLE `watched_anime`
 -- AUTO_INCREMENT for table `watched_anime`
 --
 ALTER TABLE `watched_anime`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
